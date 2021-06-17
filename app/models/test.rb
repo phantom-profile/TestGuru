32d8dbd8f9d3c12a6672b926d3bdb366413aa56a
Андрей Румянцев <Wertyos111@gmail.com>
2021-06-18 01:41:04 +0300

    completed tasks
    - added method for searching tests by title
    - added method for searching user's tests

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,2 +1,10 @@
+# frozen_string_literal: true
+
+# Tests of different categories with questions and answers
 class Test < ApplicationRecord
+
+  def self.tests_by_category(title)
+    category = Category.find_by(title: title)
+    Test.where(category_id: category.id).pluck(:title)
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,9 @@
+# frozen_string_literal: true
+
 class User < ApplicationRecord
+  has_and_belongs_to_many :tests
+
+  def completed_tests(level)
+    tests.where(level: level)
+  end
 end
