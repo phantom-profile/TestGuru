6619540f16b31d8414f39433c9dcf4f3c914411b
Андрей Румянцев <Wertyos111@gmail.com>
2021-06-18 17:00:28 +0300

    made completed_tests with JOIN

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,10 @@
 # frozen_string_literal: true
 
 class User < ApplicationRecord
-  has_and_belongs_to_many :tests
 
   def completed_tests(level)
-    tests.where(level: level)
+    Test.joins('INNER JOIN tests_users ON tests.id = tests_users.test_id')
+        .joins('INNER JOIN users ON users.id = tests_users.user_id')
+        .where('users.id': id, 'tests.level': level)
   end
 end
