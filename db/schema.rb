68d87b7364d273909e31c5cd811e057efcf415f9
Андрей Румянцев <Wertyos111@gmail.com>
2021-06-22 01:39:36 +0300

    Update schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_06_18_132715) do
+ActiveRecord::Schema.define(version: 2021_06_21_214646) do
 
   create_table "answers", force: :cascade do |t|
     t.string "body", null: false
@@ -59,6 +59,7 @@ ActiveRecord::Schema.define(version: 2021_06_18_132715) do
     t.integer "role", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "email"
   end
 
   add_foreign_key "answers", "questions"
