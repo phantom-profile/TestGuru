8e8d1907f8deae36796d42bc2c0c5c8bd100a439
Андрей Румянцев <Wertyos111@gmail.com>
2021-06-22 01:42:29 +0300

    add presence validation and number of answers

--- app/models/question.rb
+++ app/models/question.rb
@@ -6,4 +6,7 @@ class Question < ApplicationRecord
   has_many :answers, dependent: :destroy
 
   scope :correct_answers, -> { answers.where(correct: true) }
+
+  validates :body, presence: true
+  validates :answers, length: { in: 1..4 }
 end
