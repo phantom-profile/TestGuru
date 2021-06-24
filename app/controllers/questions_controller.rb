626fc98be108db39cba7bebd07236bcfcdd8b4be
Андрей Румянцев <Wertyos111@gmail.com>
2021-06-24 11:01:42 +0300

    code improvements

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class QuestionsController < ApplicationController
-  before_action :test, only: %i[index new]
+  before_action :test, only: %i[index new create destroy]
   before_action :question, only: %i[show destroy]
 
   rescue_from ActiveRecord::RecordNotFound, with: :rescue_with_question_not_found
@@ -17,26 +17,32 @@ class QuestionsController < ApplicationController
   def new; end
 
   def create
-    Question.create(params.permit(:body, :test_id))
-    redirect_to :test_questions
+    new_q = @test.questions.create(question_params)
+    render :new and return if new_q.new_record?
+
+    redirect_to new_q
   end
 
   def destroy
     @question.destroy
-    redirect_to :test_questions
+    redirect_to @test
   end
 
   private
 
   def test
-    @test = Test.find(params['test_id'])
+    @test = Test.find(params[:test_id])
   end
 
   def question
-    @question = Question.find(params['id'])
+    @question = Question.find(params[:id])
+  end
+
+  def question_params
+    params.require(:question).permit(:body, :test_id)
   end
 
   def rescue_with_question_not_found
-    render plain: "question with id: #{params['id']} was not found"
+    render plain: "question with id: #{params[:id]} was not found"
   end
 end
