42b0297e1106eeff7a2ca07284e81c1d63765427
Андрей Румянцев <Wertyos111@gmail.com>
2021-06-25 20:51:48 +0300

    added helpers

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
+# frozen_string_literal: true
+
 module ApplicationHelper
+  def show_year
+    Time.now.year
+  end
+
+  def github_url(author, repo)
+    "https://github.com/#{author}/#{repo}/"
+  end
 end
--- app/helpers/questions_helper.rb
+++ app/helpers/questions_helper.rb
@@ -1,2 +1,12 @@
+# frozen_string_literal: true
+
 module QuestionsHelper
+  def question_header(test)
+    action = if action_name == 'new'
+               'Create'
+             else
+               'Edit'
+             end
+    "#{action} #{test.title} question"
+  end
 end
