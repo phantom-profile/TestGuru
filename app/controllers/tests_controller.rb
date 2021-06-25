ef060d109a9bbb7232fce412db31559e720a2b82
Андрей Румянцев <Wertyos111@gmail.com>
2021-06-25 20:53:52 +0300

    added method show for tests controller

--- app/controllers/tests_controller.rb
+++ app/controllers/tests_controller.rb
@@ -1,2 +1,14 @@
+# frozen_string_literal: true
+
 class TestsController < ApplicationController
+
+  before_action :test, only: %i[show]
+
+  def show; end
+
+  private
+
+  def test
+    @test = Test.find(params[:id])
+  end
 end
