4e5f1a69758d856eb6edad2aa613aad5e0f74078
Андрей Румянцев <Wertyos111@gmail.com>
2021-06-26 22:55:07 +0300

    code improve

--- app/helpers/questions_helper.rb
+++ app/helpers/questions_helper.rb
@@ -1,12 +1,12 @@
 # frozen_string_literal: true
 
 module QuestionsHelper
-  def question_header(test:, question:)
+  def question_header(question)
     action = if question.new_record?
                'Create'
              else
                'Edit'
              end
-    "#{action} #{test.title} question"
+    "#{action} #{question.test.title} question"
   end
 end
