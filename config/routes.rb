8eecbe6d2c69111af024cc6405739c9f2e260e4f
Андрей Румянцев <Wertyos111@gmail.com>
2021-06-28 17:39:18 +0300

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -2,14 +2,13 @@
 
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root to: 'tests#index'
 
   resources :tests do
     resources :questions, except: [:index], shallow: true do
       resources :answers, except: [:index], shallow: true
     end
-    member do
-      post :start
-    end
+    post :start, on: :member
   end
 
   resources :test_passages, only: %i[show update] do
