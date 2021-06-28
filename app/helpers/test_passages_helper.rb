de2f5d2e471163bece0512f896cdf95570569382
Андрей Румянцев <Wertyos111@gmail.com>
2021-06-28 17:40:11 +0300

    added css styles

--- app/helpers/test_passages_helper.rb
+++ app/helpers/test_passages_helper.rb
@@ -1,16 +1,14 @@
 # frozen_string_literal: true
 
 module TestPassagesHelper
-  def percent_result(test_passage)
-    if test_passage.successful?
-      success = 'successfully'
-      color = 'green'
-    else
-      success = 'unsuccessfully'
-      color = 'red'
-    end
+  def result_of(test_passage)
+    success = if test_passage.successful?
+                'successfully'
+              else
+                'unsuccessfully'
+              end
     result = test_passage.result.to_i
-    "<font color='#{color}'> Test was completed #{success}. #{result}% of answers correct</font>".html_safe
+    "Test was completed #{success}. #{result}% of answers correct"
   end
 
   def question_number(question, questions)
