ddfc921c0f2856b01bfbad084aaa2e672d5524f5
Андрей Румянцев <Wertyos111@gmail.com>
2021-06-29 22:24:04 +0300

    added signup system

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,13 +1,20 @@
 # frozen_string_literal: true
 
 class User < ApplicationRecord
+
   has_many :written_tests, class_name: 'Test', foreign_key: :author_id, dependent: :destroy
   has_many :test_passages, dependent: :destroy
   has_many :tests, through: :test_passages
 
-  validates :email, presence: true
+  validates_presence_of :firstname, :lastname, :username
+  validates_uniqueness_of :username
+  validates :email, presence: true,
+                    uniqueness: true,
+                    format: /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i
 
   scope :completed_tests, ->(level) { tests.where(level: level) }
+
+  has_secure_password
 
   def test_passage(test)
     test_passages.order(id: :desc).find_by(test_id: test.id)
