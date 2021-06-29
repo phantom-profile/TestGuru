c626d83e4791fdc7a2b4f8a0b136b47ec50efd08
Андрей Румянцев <Wertyos111@gmail.com>
2021-06-29 22:26:52 +0300

    added two app helpers and all needed routes

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -8,4 +8,12 @@ module ApplicationHelper
   def github_url(title:, author:, repo:)
     link_to title, "https://github.com/#{author}/#{repo}/", target: :_blank
   end
+
+  def link_to_root
+    link_to 'To tests', root_path
+  end
+
+  def flash_msg(key:, tag:, style:)
+    content_tag tag, flash[key], class: style if flash[key]
+  end
 end
