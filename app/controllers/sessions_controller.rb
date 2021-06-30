9c41da8f13cbfff09e090090e6993815d82998b1
Андрей Румянцев <Wertyos111@gmail.com>
2021-06-30 13:51:32 +0300

    save redirect from nil error and cleared cookie

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,13 +1,16 @@
 # frozen_string_literal: true
 
 class SessionsController < ApplicationController
-  def new; end
+  def new
+    redirect_to root_path if current_user
+  end
 
   def create
     user = User.find_by(username: params[:username])
     if user&.authenticate(params[:password])
       session[:user_id] = user.id
-      redirect_to session[:path]
+      redirect_to session[:login_permitted_path] || root_path
+      session.delete(:login_permitted_path)
     else
       flash.now[:alert] = 'Login or password are incorrect'
       render :new
