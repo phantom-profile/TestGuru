fc2ae0c2bde8a58a9ff19ec972d53db34f0641bd
Андрей Румянцев <Wertyos111@gmail.com>
2021-07-04 00:53:16 +0300

    added gems created Gist model and moved token to secrets

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_07_02_133339) do
+ActiveRecord::Schema.define(version: 2021_07_03_193857) do
 
   create_table "answers", force: :cascade do |t|
     t.string "body", null: false
@@ -23,6 +23,14 @@ ActiveRecord::Schema.define(version: 2021_07_02_133339) do
 
   create_table "categories", force: :cascade do |t|
     t.string "title", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "gists", force: :cascade do |t|
+    t.string "question"
+    t.string "url"
+    t.string "user"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
