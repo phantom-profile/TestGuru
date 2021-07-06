1b82d502027e930c8dae4fcfe589583053ace3d2
Андрей Румянцев <Wertyos111@gmail.com>
2021-07-06 22:46:57 +0300

    removed comment

--- app/controllers/gists_controller.rb
+++ app/controllers/gists_controller.rb
@@ -4,13 +4,8 @@ class GistsController < ApplicationController
   before_action :authenticate_user!, only: [:create]
 
   def create
-    # не совсем понимаю, какие задачи тут лишние
-    # 1) создается json для Gist под контролем CreateGist
     @test_passage = TestPassage.find(params[:test_passage_id])
     result = Github::CreateGist.new(@test_passage.current_question).call
-
-    # 2) в случае успеха контроль передается модели Gist и в обоих случаях создается флэшка для сообщения
-    # после редиректа. Какой функционал стоит убрать из метода?
     if result.success?
       Gist.create(question: @test_passage.current_question.body,
                   user: current_user.email,
