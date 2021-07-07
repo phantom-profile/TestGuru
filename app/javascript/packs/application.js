7a146fef244a8c4936ab2a97ae415ce90f4309be
Андрей Румянцев <Wertyos111@gmail.com>
2021-07-07 21:03:45 +0300

    Update application.js

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,7 +7,7 @@ import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
-require("utilities/sorting")
+require("packs/sorting")
 require("devise/password_match_alert")
 
 Rails.start()
