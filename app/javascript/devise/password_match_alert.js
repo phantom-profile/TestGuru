db6b84ec6d63c9e04df596bce0bce59803d603b2
Андрей Румянцев <Wertyos111@gmail.com>
2021-07-08 13:03:40 +0300

    code fixes

--- app/javascript/devise/password_match_alert.js
+++ app/javascript/devise/password_match_alert.js
@@ -2,7 +2,7 @@ import PasswordMatchMessage from './password_match_message_class'
 
 document.addEventListener('turbolinks:load', function () {
   let passwordFields = document.querySelectorAll('.match-password')
-  if (passwordFields) {
+  if (passwordFields.length) {
     let passwordMessage = new PasswordMatchMessage(passwordFields)
     passwordMessage.catchEvents(passwordFields)
   }
