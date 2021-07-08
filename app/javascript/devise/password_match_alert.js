845ab89f45145d60f176988eef83d8d38b9bf846
Андрей Румянцев <Wertyos111@gmail.com>
2021-07-08 11:38:03 +0300

    moved catching events to class methods and moved class to separated file

--- app/javascript/devise/password_match_alert.js
+++ app/javascript/devise/password_match_alert.js
@@ -1,35 +1,9 @@
+import PasswordMatchMessage from './password_match_message_class'
+
 document.addEventListener('turbolinks:load', function () {
   let passwordFields = document.querySelectorAll('.match-password')
   if (passwordFields) {
-    passwordFields.forEach(function (field) {
-      field.addEventListener(
-        'input',
-        function () {
-          let matchMessage = new PasswordMatchMessage(passwordFields)
-          matchMessage.displayMessage()
-        }
-      )
-    })
+    let passwordMessage = new PasswordMatchMessage(passwordFields)
+    passwordMessage.catchEvents(passwordFields)
   }
 })
-
-class PasswordMatchMessage {
-  constructor(fields) {
-    this.password = fields[0]
-    this.passwordConfirmation = fields[1]
-
-    this.match = this.passwordConfirmation.parentNode.querySelector('.notice')
-    this.notMatch = this.passwordConfirmation.parentNode.querySelector('.alert')
-  }
-
-  displayMessage() {
-    console.log(this.notMatch, this.match)
-    if (this.password.value !== this.passwordConfirmation.value) {
-      this.notMatch.classList.remove('hide')
-      this.match.classList.add('hide')
-    } else {
-      this.match.classList.remove('hide')
-      this.notMatch.classList.add('hide')
-    }
-  }
-}
---
+++ app/javascript/devise/password_match_message_class.js
@@ -0,0 +1,30 @@
+export default class PasswordMatchMessage {
+
+    constructor(fields) {
+
+        this.password = fields[0]
+        this.passwordConfirmation = fields[1]
+
+        this.match = this.passwordConfirmation.parentNode.querySelector('.notice')
+        this.notMatch = this.passwordConfirmation.parentNode.querySelector('.alert')
+    }
+
+    catchEvents(fields) {
+        this.password.addEventListener('input', () => this.displayMessage(fields))
+        this.passwordConfirmation.addEventListener('input', () => this.displayMessage(fields))
+    }
+
+    displayMessage(fields) {
+        this.password = fields[0]
+        this.passwordConfirmation = fields[1]
+
+        console.log(this.notMatch, this.match)
+        if (this.password.value !== this.passwordConfirmation.value) {
+            this.notMatch.classList.remove('hide')
+            this.match.classList.add('hide')
+        } else {
+            this.match.classList.remove('hide')
+            this.notMatch.classList.add('hide')
+        }
+    }
+}
