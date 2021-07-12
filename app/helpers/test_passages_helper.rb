6fd8361b7d664057a147affdceeb92bac65d8aab
Андрей Румянцев <Wertyos111@gmail.com>
2021-07-12 17:16:50 +0300

    add failure in case time is over

--- app/helpers/test_passages_helper.rb
+++ app/helpers/test_passages_helper.rb
@@ -4,6 +4,8 @@ module TestPassagesHelper
   def result_of(test_passage)
     success = test_passage.successful? ? 'successfully' : 'unsuccessfully'
     result = test_passage.result.to_i
-    "Test was completed #{success}. #{result}% of answers correct"
+    answer = "Test was completed #{success}. #{result}% of answers correct"
+    answer += ' Time runs out' if test_passage.time_runs_out
+    answer
   end
 end
