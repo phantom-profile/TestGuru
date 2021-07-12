1b3cbe3d96f37389253bb45cbd28aef0d9848d5b
Андрей Румянцев <Wertyos111@gmail.com>
2021-07-12 17:16:13 +0300

    add time attribute to test

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_07_03_193857) do
+ActiveRecord::Schema.define(version: 2021_07_12_095345) do
 
   create_table "answers", force: :cascade do |t|
     t.string "body", null: false
@@ -62,6 +62,7 @@ ActiveRecord::Schema.define(version: 2021_07_03_193857) do
     t.integer "author_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "time", default: 10, null: false
     t.index ["author_id"], name: "index_tests_on_author_id"
     t.index ["category_id"], name: "index_tests_on_category_id"
   end
