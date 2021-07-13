5789f90eb0862f33fe8710b662516a6b539986f9
Андрей Румянцев <Wertyos111@gmail.com>
2021-07-13 16:40:29 +0300

    created db platform for badges
    1) created badges table
    2) connected it to users table by m2m (1 user has many achievements, 1 achievement had bu many users)
    3) added seeds and default picture

---
+++ db/migrate/20210713131058_create_badges.rb
@@ -0,0 +1,9 @@
+class CreateBadges < ActiveRecord::Migration[6.1]
+  def change
+    create_table :badges do |t|
+      t.string :title, null: false
+      t.string :image_link, null: false, default: '/app/assets/img.png'
+      t.timestamps
+    end
+  end
+end
